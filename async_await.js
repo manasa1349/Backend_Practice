8bb4acc8416cc882974d5d264c618283596c8373
manasa1349 <charmymanasa@gmail.com>
2025-12-04 10:49:32 +0530

    modified async_await

--- async_await.js
+++ async_await.js
@@ -1,14 +1,15 @@
-function getUserDetails(){
+function getUserDetails(id){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve({userRoll:"456"})
+            console.log(id)
         },2000);
     })
 };
 
 const myfun=async()=>{
     console.log("123");
-    const result=await getUserDetails();
+    const result=await getUserDetails(555);
     console.log(result);
     console.log("789");
 };
